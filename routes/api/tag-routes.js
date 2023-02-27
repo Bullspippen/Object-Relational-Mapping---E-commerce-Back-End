62fa790620535efa591456b6106680a9135aae2b
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-26 23:55:34 -0500

    tag-routes.js updated to include message tag deleted successfully

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -78,7 +78,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(tag);
+    res.status(200).json({ message: 'Tag Deleted Successfully' });
   } catch (err) {
     res.status(500).json(err);
   }
